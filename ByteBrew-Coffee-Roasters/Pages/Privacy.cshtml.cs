e079fa94d8951f89600bf5954514ce9c6e5dcb57
Александр <1423demon@mail.ru>
2024-03-27 22:13:58 +0300

    init

---
+++ ByteBrew-Coffee-Roasters/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ByteBrew_Coffee_Roasters.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
