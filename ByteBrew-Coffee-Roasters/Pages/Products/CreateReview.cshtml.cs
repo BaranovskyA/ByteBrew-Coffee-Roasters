c725916070f655ba7cf6270f666f9482db86f01a
Apollyon <52245082+AveApollyon@users.noreply.github.com>
2024-05-16 22:54:31 +0300

    orders + pages titles fix + easter egg

---
+++ ByteBrew-Coffee-Roasters/Pages/Products/CreateReview.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ByteBrew_Coffee_Roasters.Data;
+using ByteBrew_Coffee_Roasters.Data.Models;
+
+namespace ByteBrew_Coffee_Roasters.Pages.Products
+{
+    public class CreateReviewModel : PageModel
+    {
+        private readonly ByteBrew_Coffee_Roasters.Data.ApplicationDbContext _context;
+
+        public CreateReviewModel(ByteBrew_Coffee_Roasters.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public ProductReview ProductReview { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.ProductReviews.Add(ProductReview);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
